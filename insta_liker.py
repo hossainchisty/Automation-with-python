aa7e42b6f6b07ba76aef1c17026018fa428b0b4c
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2020-10-09 22:03:09 +0600

    Add files via upload

---
+++ insta_liker.py
@@ -0,0 +1,13 @@
+import pyautogui
+from time import sleep
+pyautogui.FAILSAFE = False
+while True:
+    sleep(0.10)
+    pyautogui.doubleClick()
+    sleep(0.2) 
+    pyautogui.scroll(-300)
+    
+    
+
+    
+    
